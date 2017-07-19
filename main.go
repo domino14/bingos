14283ea48d35bbb4f269ae10c851248ec5501ef1
Cesar Del Solar <delsolar@gmail.com>
2017-07-19 10:56:10 -0700

    Adding type 2 and 3s

--- main.go
+++ main.go
@@ -10,7 +10,26 @@ import (
 	"github.com/domino14/macondo/lexicon"
 )
 
-var dawgPath = flag.String("dawgpath", "", "path for dawgs")
+var (
+	// Flagged values.
+	dawgPath   = flag.String("dawgpath", "", "path for dawgs")
+	searchType = flag.String("type", "type1",
+		"the type of search - type1, type2, type3, stems")
+	// Type 1 is bingos in the top 100 stems by MMPR. Unfortunately this
+	// changed since OWL2..
+	// Type 2 is the letters in U DATE NO GIRLS
+	// with the following repetition frequencies
+	// A(3) D(1) E(4) G(1) I(3) L(1) N(2) O(2) R(2) S(2) T(2) U(1)
+	// Type 3 is 7s with >= probability than HUNTERS and 8s >= NOTIFIED
+	// (why NOTIFIED? ask M.B...)
+	// Stems generates a list of stems of length `searchLength - 1`, sorted
+	// by MMPR.
+
+	topNStems    = flag.Int("topn", 100, "top N stems of this length")
+	searchLength = flag.Int("length", 7, "length of word")
+	stemLexicon  = flag.String("lexicon", "America", "name of lexicon")
+)
+
 var defaultDist lexicon.LetterDistribution
 
 type Stem struct {
@@ -22,29 +41,142 @@ type Stem struct {
 }
 
 func (s *Stem) Printable(order int) string {
-	return fmt.Sprintf("%v\t%v\t%.3f\t%v\t%.3f (combos %v)", order, s.Alphagram,
+	return fmt.Sprintf("%v\t%v\t%.4f\t%v\t%.4f\t%v", order, s.Alphagram,
 		s.ModifiedStemProbability, s.UsableTiles, s.MMPR, s.StemCombinations)
 }
-
-var stemMap map[string]map[rune]bool
 
 func main() {
 
 	flag.Parse()
+	if *searchLength != 7 && *searchLength != 8 {
+		fmt.Println("Error - stem length must be 7 or 8")
+		return
+	}
 	anagrammer.LoadDawgs(*dawgPath)
 	defaultDist = lexicon.EnglishLetterDistribution()
-	stemMap = make(map[string]map[rune]bool)
 
-	sevens := anagrammer.Anagram("???????", anagrammer.Dawgs["America"],
-		anagrammer.ModeExact)
+	wordPrinter := func(words []string) {
+		for _, word := range words {
+			fmt.Println(word)
+		}
+		fmt.Println(len(words))
+	}
 
-	for _, seven := range sevens {
-		stemDecompose(seven)
+	if *searchType == "type1" {
+		words, _ := calculateTypeIs(*searchLength)
+		for _, word := range words {
+			fmt.Println(word)
+		}
+		fmt.Println(len(words))
+	} else if *searchType == "type2" {
+		words := calculateTypeIIs(*searchLength)
+		wordPrinter(words)
+
+	} else if *searchType == "type3" {
+		words := calculateTypeIIIs(*searchLength)
+		wordPrinter(words)
+
+	} else if *searchType == "stems" {
+		stems := calculateStems(*searchLength, *topNStems)
+		fmt.Println("#\talpha\tMSP\tUT\tMMPR\tstemcombos")
+		for idx, stem := range stems {
+			fmt.Println(stem.Printable(idx + 1))
+		}
 	}
-	stems := processStems()
-	for idx, stem := range stems[:10000] {
-		fmt.Println(stem.Printable(idx + 1))
+}
+
+// calculate Type I bingos and put them in a map.
+// Type I 7s are made from the top n 6 letter stems
+// Type I 8s are also made from the same stems + 2 letters.
+func calculateTypeIs(length int) ([]string, map[string]bool) {
+	stems := calculateStems(7, 100)
+	wordMap := map[string]bool{}
+	blanks := "?"
+	if length == 8 {
+		blanks = "??"
 	}
+	for _, stem := range stems {
+		theseWords := anagrammer.Anagram(stem.Alphagram+blanks,
+			anagrammer.Dawgs[*stemLexicon], anagrammer.ModeExact)
+		for _, word := range theseWords {
+			wordMap[word] = true
+		}
+	}
+	words := []string{}
+	for word := range wordMap {
+		words = append(words, word)
+	}
+	sort.Strings(words)
+	return words, wordMap
+}
+
+func calculateTypeIIs(length int) []string {
+	_, type1s := calculateTypeIs(length)
+	ret := []string{}
+	//A(3) D(1) E(4) G(1) I(3) L(1) N(2) O(2) R(2) S(2) T(2) U(1)
+	words := anagrammer.Anagram("AAADEEEEGIIILNNOORRSSTTU",
+		anagrammer.Dawgs[*stemLexicon], anagrammer.ModeBuild)
+	for _, word := range words {
+		if len(word) == length {
+			_, ok := type1s[word]
+			if !ok {
+				ret = append(ret, word)
+			}
+		}
+	}
+	sort.Strings(ret)
+	return ret
+}
+
+func calculateTypeIIIs(length int) []string {
+	var refCombos uint64
+	lexInfo := lexicon.LexiconInfo{
+		LetterDistribution: lexicon.EnglishLetterDistribution(),
+	}
+	lexInfo.Initialize()
+	if length == 7 {
+		refCombos = lexInfo.Combinations("EHNRSTU", false)
+	} else if length == 8 {
+		refCombos = lexInfo.Combinations("DEFIINOT", false)
+	}
+	_, wordMap := calculateTypeIs(length)
+	typeIIs := calculateTypeIIs(length)
+	// insert typeIIs to word map
+	for _, word := range typeIIs {
+		wordMap[word] = true
+	}
+	typeIIIs := []string{}
+
+	for _, word := range allWords(length) {
+		// Check if it's not in map.
+		_, ok := wordMap[word]
+		if !ok {
+			if lexInfo.Combinations(word, false) >= refCombos {
+				typeIIIs = append(typeIIIs, word)
+			}
+		}
+	}
+	sort.Strings(typeIIIs)
+	return typeIIIs
+}
+
+func allWords(length int) []string {
+	return anagrammer.Anagram(strings.Repeat("?", length),
+		anagrammer.Dawgs[*stemLexicon], anagrammer.ModeExact)
+}
+
+// Calculate the top `stemCutoff` stems of length `length - 1`
+func calculateStems(length int, stemCutoff int) []Stem {
+	words := allWords(length)
+	stemMap := map[string]map[rune]bool{}
+	for _, word := range words {
+		stemDecompose(word, stemMap)
+	}
+	stems := processStems(stemMap)
+	if stemCutoff > len(stems)-1 {
+		stemCutoff = len(stems) - 1
+	}
+	return stems[:stemCutoff]
 }
 
 func modifiedS(dist lexicon.LetterDistribution) lexicon.LetterDistribution {
@@ -56,7 +188,7 @@ func modifiedS(dist lexicon.LetterDistribution) lexicon.LetterDistribution {
 
 // Decompose word into all substrings of word length less than 1, and add
 // letters to stem map.
-func stemDecompose(word string) {
+func stemDecompose(word string, stemMap map[string]map[rune]bool) {
 	// w := lexicon.Word{Word: word, Dist: defaultDist}
 	// alphagram := w.MakeAlphagram()
 
@@ -71,25 +203,34 @@ func stemDecompose(word string) {
 	}
 }
 
+// Calculates the usable tiles for a stem alphagram. Assumes tileMap
+// has at least one letter in it.
 func calcUsableTiles(stemAlpha string, tileMap map[rune]bool) uint8 {
-	n := uint8(0)
+	n := int8(0)
+	if len(tileMap) == 0 {
+		panic("stemAlpha invalid: " + stemAlpha)
+	}
 	for rn := range tileMap {
-		n += defaultDist.Distribution[rn] - uint8(strings.Count(stemAlpha, string(rn)))
+		n += int8(defaultDist.Distribution[rn]) - int8(strings.Count(stemAlpha, string(rn)))
 	}
-	if n > 0 {
-		n += 2 // The blanks
-	}
-	return n
+	n += 2 // The blanks
+
+	return uint8(n)
 }
 
 // Take stemMap and create a sorted slice of stems.
-func processStems() []Stem {
+func processStems(stemMap map[string]map[rune]bool) []Stem {
 	lexInfo := lexicon.LexiconInfo{
 		LetterDistribution: modifiedS(lexicon.EnglishLetterDistribution()),
 	}
 	lexInfo.Initialize()
 
-	tisane := lexInfo.Combinations("AEINST", false)
+	var baseStemCombos uint64
+	if *searchLength == 7 {
+		baseStemCombos = lexInfo.Combinations("AEINST", false)
+	} else if *searchLength == 8 {
+		baseStemCombos = lexInfo.Combinations("AEINRST", false)
+	}
 	stems := []Stem{}
 	for stemAlpha, tiles := range stemMap {
 		s := Stem{}
@@ -97,7 +238,7 @@ func processStems() []Stem {
 		s.Alphagram = stemAlpha
 		s.UsableTiles = calcUsableTiles(stemAlpha, tiles)
 		s.StemCombinations = lexInfo.Combinations(stemAlpha, false)
-		s.ModifiedStemProbability = 1.5 * (float64(s.StemCombinations) / float64(tisane))
+		s.ModifiedStemProbability = 1.5 * (float64(s.StemCombinations) / float64(baseStemCombos))
 		s.MMPR = s.ModifiedStemProbability * float64(s.UsableTiles)
 		stems = append(stems, s)
 	}
